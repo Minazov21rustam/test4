71e74afc5eaf5a2987da9e3a6ebf647f34a4b8ad
Rustam <rustam2001.mr@gmail.com>
2022-12-13 20:47:53 +0400

    1

---
+++ films/src/pages/ListPage/ListPage.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./ListPage.css";
+
+function ListPage() {
+  const [movies] = useState([
+    { title: "The Godfather", year: 1972, imdbID: "tt0068646" },
+  ]);
+
+  const params = useParams();
+
+  useEffect(() => {
+    // const  = match.params;
+    console.log(params?.id);
+    // TODO: запрос к сервер на получение списка
+    // TODO: запросы к серверу по всем imdbID
+  });
+  return (
+    <div className="list-page">
+      <h1 className="list-page__title">Мой список</h1>
+      <ul>
+        {this.state.movies.map((item) => {
+          return (
+            <li key={item.imdbID}>
+              <a href="https://www.imdb.com/title/tt0068646/" target="_blank">
+                {item.title} ({item.year})
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default ListPage;
